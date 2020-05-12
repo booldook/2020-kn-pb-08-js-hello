c52f96b12af445f9919ffedd1a6a9f74639b524f
booldook <booldook@gmail.com>
2020-05-12 12:06:16 +0900

    12.score

---
+++ js/12.score.js
@@ -0,0 +1,28 @@
+/* 본 프로그램을 위한 사전 지식 */
+var arr = [];
+arr[0] = 10;
+arr[1] = {
+	name: "홍길동",
+	kor: 90,
+	eng: 80,
+	math: 70,
+}
+console.log(arr);
+
+var name = "홍길동";
+
+/* 변수선언 */
+var std = [];
+
+
+/* btSave를 클릭시 */
+$("#btSave").click(function(){
+	std.push({
+		name: $("#name").val(),
+		kor: $("#kor").val(),
+		eng: $("#eng").val(),
+		math: $("#math").val(),
+	});
+	var html = '<tr><td>1</td><td>홍길동</td><td>89</td><td>80</td><td>70</td></tr>';
+	$(".score-tb > tbody").append(html);
+});
