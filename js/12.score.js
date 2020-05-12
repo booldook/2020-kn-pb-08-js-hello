08f61532b4bb69a65aa68839f271218ec82246d6
booldook <booldook@gmail.com>
2020-05-12 13:05:10 +0900

    12.score 만드는 중

--- js/12.score.js
+++ js/12.score.js
@@ -13,11 +13,26 @@ var name = '홍길동';
 var hello = '안녕하세요 저는 "' + name + '" 입니다.';
 console.log(hello);
 
+var i = 10;
+i = i + 1;
+i++;
+i += 1;
+console.log(i); 
+
+var j = 10;
+j += 5; // j = j + 5;
+
+var html = '';
+html += '<tr>'; 									// <tr>
+html += '<td>' + j + '</td>'; 		// <tr><td>10</td>
+html += '</tr>'; 									// <tr><td>10</td></tr>
+console.log(html);
+
+
 
 
 /* 변수선언 */
 var std = [];
-
 
 /* btSave를 클릭시 */
 $("#btSave").click(function(){
@@ -27,6 +42,15 @@ $("#btSave").click(function(){
 		eng: $("#eng").val(),
 		math: $("#math").val(),
 	});
-	var html = '<tr><td>1</td><td>홍길동</td><td>89</td><td>80</td><td>70</td></tr>';
+	var html = '';
+	html += '<tr>';
+	html += '	<td>1</td>';
+	html += '	<td>'+$("#name").val()+'</td>';
+	html += '	<td>'+$("#kor").val()+'</td>';
+	html += '	<td>'+$("#eng").val()+'</td>';
+	html += '	<td>'+$("#math").val()+'</td>';
+	html += '	<td>240</td>';
+	html += '	<td>80</td>';
+	html += '</tr>';
 	$(".score-tb > tbody").append(html);
 });
