9da5f2900e1d52e0d90545081341e7a1af9c02a9
booldook <booldook@gmail.com>
2020-05-13 11:47:26 +0900

    15.gallery

---
+++ js/15.gallery.js
@@ -0,0 +1,40 @@
+/*
+<div class="list">
+	<img src="../img/p1.jpg" alt="p1.jpg" class="photo img">
+	<div class="photo-desc">그림 설명</div>
+</div>
+*/
+var lists = [];
+lists[0] = {
+	title: "그림1",
+	img: '../img/p1.jpg'
+}
+lists[1] = {
+	title: "그림2",
+	img: '../img/p2.jpg'
+}
+lists[2] = {
+	title: "그림3",
+	img: '../img/p3.jpg'
+}
+lists[3] = {
+	title: "그림4",
+	img: '../img/p4.jpg'
+}
+lists[4] = {
+	title: "그림5",
+	img: '../img/p5.jpg'
+}
+lists[5] = {
+	title: "그림6",
+	img: '../img/p6.jpg'
+}
+lists[6] = {
+	title: "그림7",
+	img: '../img/p7.jpg'
+}
+lists[7] = {
+	title: "그림8",
+	img: '../img/p8.jpg'
+}
+
