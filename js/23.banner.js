ae9182df37f37fa09243588bfe94cd83e7d77200
booldook <booldook@gmail.com>
2020-05-20 12:21:13 +0900

    23.banner 만드는 중

--- js/23.banner.js
+++ js/23.banner.js
@@ -14,26 +14,40 @@ document.getElementById("bt").addEventListener("click", function(){
 $("#bt").click(function(){
 	console.log("hi~");
 });
+
+css("color")
+css("color", "red")
 */
 
 /************* 초기값 *************/
 var now = 0;
 var $ban = $(".ban");
-console.log($($ban[0]))
 var last = $ban.length - 1;
 init();
 
 /************* 사용자 지정 *************/
 function init() {
 	$(".ban-wrap").empty();
-	//console.log($ban[now]);
+	html = '<img src="'+$($ban[now]).children("img").attr("src")+'" class="img">';
+	$(".ban-wrap").append(html);
 }
 
-
+function ani() {
+	// console.log(	$(".ban-wrap").prepend($ban[now])	);
+	// console.log(	$($ban[now]).prependTo($(".ban-wrap"))	);
+}
 
 /************* 이벤트 등록 *************/
-
-
-
+$(".bt-prev").click(onPrev);
+$(".bt-next").click(onNext);
 
 /************* 이벤트 콜백 *************/
+function onPrev() {
+	now = (now == 0) ? last : now - 1;
+	ani();
+}
+
+function onNext() {
+	now = (now == last) ? 0 : now + 1;
+	ani();
+}
