258d606d473deec279c08670f13308ed6e8a0888
booldook <booldook@gmail.com>
2020-05-20 10:41:11 +0900

    22.banner

--- js/22.banner.js
+++ js/22.banner.js
@@ -1,3 +1,23 @@
+/******** 사전지식 ********/
+var arr = [1000, 999, 998, 997];
+// 1. 배열의 맨 뒤의 값을 꺼내기
+var temp = arr.pop();
+console.log(arr, temp);
+
+// 2. 배열의 맨 뒤의 값을 넣기
+arr.push(997);
+console.log(arr);
+
+// 3. 배열의 맨 앞에서 값을 꺼내기
+temp = arr.shift();
+console.log(arr, temp); 
+
+// 3. 배열의 맨 앞에서 값을 넣기
+arr.unshift(1000);
+console.log(arr); 
+
+
+
 /*
 - bt-next click
 1. opacity: 1 -> 0
@@ -20,7 +40,6 @@ function init() {
 		idx[i] = depth;
 		$(".ban").eq(i).css("z-index", depth--);
 	}
-	console.log(idx);
 }
 
 /******** 이벤트 등록 ********/
