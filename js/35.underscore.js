515aae2655e432b421383eeb25ed9c94bbf88a84
booldook <booldook@gmail.com>
2020-06-05 11:19:35 +0900

    35.underscore

---
+++ js/35.underscore.js
@@ -0,0 +1,33 @@
+var lotto = [];
+for(var i=0; i<45; i++) lotto.push(i+1);
+
+function onNumbers() {
+	var getLotto = [];
+	// 여기가 추출알고리즘 [22, 16, 33, 34, 1, 9]
+	
+	console.log(getLotto);
+}
+
+$(".bt-lotto").click(onNumbers);
+
+
+
+
+
+
+
+
+
+
+
+function lottoMaker(arr) {
+var html = '';
+	/* <li class="lotto">
+	<span class="yellow">1</span>
+	<span class="blue">13</span>
+	<span class="red">26</span>
+	<span class="gray">33</span>
+	<span class="green">42</span>
+	<span class="green">44</span>
+	</li> */
+}
