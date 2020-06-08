cf563f5a4fda44329b78fd844c85c3e04f8f6c3d
booldook <booldook@gmail.com>
2020-06-08 11:21:01 +0900

    jQUery 선택자

---
+++ js/38.ready.js
@@ -0,0 +1,13 @@
+console.log($(".wrapper").html());
+
+console.log(	$("#LastName, .intro")	);
+console.log(	$(".intro")	);
+console.log(	$(".intro", "#LastName")	);
+console.log(	$("#LastName").find(".intro")	);
+
+
+
+
+$(document).ready(function () {
+
+});
