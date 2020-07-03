d0493de38581943d6c27d0dcf574a694a3005ffc
booldook <booldook@gmail.com>
2020-07-03 15:13:45 +0900

    48.slide 반응형 완성

--- js/48.slide.js
+++ js/48.slide.js
@@ -1,26 +1,29 @@
 var now = 0;
-var tar = 0;
 var el = $(".slide");
-var last = el.length - 1;
-var arr = [3,4,5];
+var last = el.length - 1;	// 마지막 슬라이드 index
+var slideMaxLen = 6; // 화면에 나올 최대 슬라이드갯수 + 2
+var arr = [];
+var lt = 0;
+var tar = 0;
 
 for(var i=0; i<el.length; i++) {
 	$(".pager-wrap").append('<i class="pager fa-circle"></i> ');
 }
 
+$(window).resize(onResize).trigger("resize");
 $(".bt-prev").click(onPrev);
 $(".bt-next").click(onNext);
 $(".pager").click(onPager);
 
-init();
-
 function init() {
-	$(".slide-wrap").css("left", "-100%");
+	$(".slide-wrap").css("left", -lt+"%");
 	$(".slide").remove();
 	arr = [];
 	arr[1] = now;
 	arr[0] = (now == 0) ? last : now - 1;
-	arr[2] = (now == last) ? 0 : now + 1;
+	for(var i=2; i<slideMaxLen; i++) {
+		arr[i] = (arr[i-1] == last) ? 0 : arr[i-1] + 1;
+	}
 	for(var i in arr) {
 		$(el[arr[i]]).clone().appendTo(".slide-wrap");
 	}
@@ -36,7 +39,7 @@ function onPrev() {
 
 function onNext() {
 	now = (now == last) ? 0 : now + 1;
-	tar = "-200%";
+	tar = -lt*2+"%";
 	ani();
 }
 
@@ -47,3 +50,21 @@ function onPager() {
 function ani() {
 	$(".slide-wrap").stop().animate({"left": tar}, 300, init);
 }
+
+function onResize() {
+	var wid = $(this).innerWidth();
+	if(wid > 991) {	// ~ 992
+		lt = 25;
+	}
+	else if(wid > 767) {	// 991 ~ 768
+		lt = 33.3333;
+	}
+	else if(wid > 575) {	// 767 ~ 575
+		lt = 50;
+	}
+	else {	// 575 ~ 0
+		lt = 100;
+	}
+	init();
+}
+
