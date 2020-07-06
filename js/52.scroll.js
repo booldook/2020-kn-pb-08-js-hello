aa024dbb2afaaf12c78f94d6dd0c6c4fa25ce1e4
booldook <booldook@gmail.com>
2020-07-06 13:19:31 +0900

    52.scroll

--- js/52.scroll.js
+++ js/52.scroll.js
@@ -14,19 +14,33 @@ function onResize() {
 
 function onScroll() {
 	var sctop = $(this).scrollTop();
-	for(var i=0; i<pageOffset.length; i++) {
-		if(pageOffset[i] + $(".page").innerHeight() >= sctop) {
+	for(var i=pageOffset.length; i>=0; i--) {
+		if(sctop >= pageOffset[i]) {
 			idx = i;
 			break;
 		}
 	}
+	console.log(idx);
 	$(".nav-link").removeClass("active");
 	$(".nav-link").eq(idx).addClass("active");
 }
 
 function onNavClick() {
 	idx = $(this).index();
-	$("html, body").stop().animate({"scrollTop": pageOffset[idx]+"px"}, 500);
+	$("html, body").stop().animate({"scrollTop": (pageOffset[idx]+1) + "px"}, 500);
 	$(".nav-link").removeClass("active");
 	$(".nav-link").eq(idx).addClass("active");
 }
+
+var interval;
+document.querySelector(".box").addEventListener("click", function(){
+	clearInterval(interval);
+	var obj = this;
+	var tar = 0;
+	interval = setInterval(function(){
+		var left = Number(obj.style.left.replace("px", ""));
+		tar += (1200 - left) * 0.05;
+		console.log(tar);
+		obj.style.left = tar + "px";
+	}, 33);
+});
